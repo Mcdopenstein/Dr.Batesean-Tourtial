6b5e95636e661bb3515cdf490267b4fdc432be93
- Austin <71462544+Mcdopenstein@users.noreply.github.com>
2021-07-04 17:03:39 -0500

    Add files via upload
    This project was complicated for me since I could not figure out how to string more than on constant together. After looking on youtube and seeing the source code and comparing what was wrong with mine I found that the original poster was doing the same thing I was. By creating a constant that could be called upon it provided the function and display I was looking for. This was fun

---
+++ Javascript/_a1.js
@@ -0,0 +1,9 @@
+const first = prompt("What is your first name?");
+const last = prompt("What is your last name?");
+const age = prompt("what is your age?");
+
+const string1 = "Hello, your name is " + first + " " + last + ". You are currently " + age + " years old.";
+
+console.log (string1)
+
+alert(string1);
