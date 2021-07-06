5463924e2ab64b83dd4f966941671127de331228
- Austin <71462544+Mcdopenstein@users.noreply.github.com>
2021-07-05 22:15:00 -0500

    Add files via upload

---
+++ Javascript/Assigment 4/_a4.js
@@ -0,0 +1,54 @@
+// let i = -7;
+// while(i <= 24){
+//   console.log([i]) + i;
+//   i++;
+// }
+
+
+// let num = 4;
+// while(num <= 30) {
+//   if(num % 2 == 0)
+//   console.log(num);
+//   num++;
+// }
+
+
+// let num = 4000;
+// while(num <= 4037) {
+//   if(num % 2 !=0)
+//   console.log(num);
+//   num++
+// }
+
+
+// let num = 5;
+// while (num <= 60){
+//   if(num % 5 == 0)
+//   if(num % 3 == 0)
+//   console.log(num);
+//   num++;
+// }
+
+
+// for(i = -7; i <= 24; i++){
+//   console.log(i);
+// }
+
+
+// for(i = 4; i <= 30; i++){
+//   if(i % 2 == 0)
+//   console.log(i);
+// }
+
+
+// for(i = 4000; i < 4037; i++){
+//   if(i % 2 != 0)
+//     console.log(i);
+// }
+
+
+// // for(i = 5; i <= 60; i++){
+//   if(i % 3 == 0)
+//   if(i % 5 == 0)
+//     console.log(i);
+//   }
