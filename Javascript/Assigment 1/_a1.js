285537b71c5b1682379582910ee16014a4f0ef38
- Austin <71462544+Mcdopenstein@users.noreply.github.com>
2021-07-08 15:27:27 -0500

    Delete Javascript directory

--- Javascript/Assigment 1/_a1.js
+++
@@ -1,9 +0,0 @@
-const first = prompt("What is your first name?");
-const last = prompt("What is your last name?");
-const age = prompt("what is your age?");
-
-const string1 = "Hello, your name is " + first + " " + last + ". You are currently " + age + " years old.";
-
-console.log = string1;
-
-alert(string1);
